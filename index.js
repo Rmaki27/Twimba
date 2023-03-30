13044e3ead141d968d78cf2ea8e58d83a5a58d7b
Rmaki27 <engineermaki27@gmail.com>
2023-03-29 23:40:48 -0700

    Add functionality to like icon and increment likes

--- index.js
+++ index.js
@@ -7,6 +7,21 @@ tweetBtn.addEventListener('click', function () {
     console.log(tweetInput.value)
 })
 
+document.addEventListener("click", function (e) {
+    if (e.target.dataset.like) {
+        handleLikeClick(e.target.dataset.like)
+    }
+})
+
+function handleLikeClick(tweetId) {
+    const targetTweetObj = tweetsData.filter(function (tweet) {
+        return tweet.uuid === tweetId
+    })[0]
+
+    targetTweetObj.likes++
+    console.log(targetTweetObj.likes)
+}
+
 function getFeedHtml(data) {
     let feedHtml = ``
 
@@ -20,12 +35,15 @@ function getFeedHtml(data) {
                     <p class="tweet-text">${tweet.tweetText}</p>
                     <div class="tweet-details">
                         <span class="tweet-detail">
+                            <i class="fa-regular fa-comment-dots" data-reply="${tweet.uuid}"></i>
                             ${tweet.replies.length}
                         </span>
                         <span class="tweet-detail">
+                        <i class="fa-solid fa-heart" data-like="${tweet.uuid}"></i>
                             ${tweet.likes}
                         </span>
                         <span class="tweet-detail">
+                        <i class="fa-solid fa-retweet" data-retweet="${tweet.uuid}"></i>
                             ${tweet.retweets}
                         </span>
                     </div>   
